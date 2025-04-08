e4a2e2622b50342fc73be0185788a9e05d9bf004
ekko <1146563605@qq.com>
2025-04-08 20:49:07 +0800

    feat: user + category

--- service/user.go
+++ service/user.go
@@ -18,21 +18,21 @@ func NewUserService(UserStore *store.UserStore) *UserService {
 }
 
 // Register -
-func (s *UserService) Register(user *model.User) error {
-	return s.UserStore.Register(user)
+func (s *UserService) Register(userReq *model.RegisterUserReq) (user model.User, err error) {
+	return s.UserStore.Register(userReq)
 }
 
 // Modify -
-func (s *UserService) Modify(modifyUserReq *model.ModifyUser) (user model.User, err error) {
+func (s *UserService) Modify(modifyUserReq *model.ModifyUserReq) (user model.User, err error) {
 	return s.UserStore.Modify(modifyUserReq)
 }
 
 // Get -
-func (s *UserService) Get(userReq *model.GetUser) (user model.User, err error) {
+func (s *UserService) Get(userReq *model.GetUserReq) (user model.User, err error) {
 	return s.UserStore.Get(userReq)
 }
 
 // Delete -
-func (s *UserService) Delete(userReq *model.DeleteUser) error {
+func (s *UserService) Delete(userReq *model.DeleteUserReq) error {
 	return s.UserStore.Delete(userReq)
 }
