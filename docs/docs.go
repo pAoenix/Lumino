c224098a5655ce335e87ef0a5d591c231408a287
ekko <1146563605@qq.com>
2025-04-16 16:33:06 +0800

    feat: delete user,update friend

--- docs/docs.go
+++ docs/docs.go
@@ -1769,7 +1769,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "182.92.152.108:8080",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Lumino",
