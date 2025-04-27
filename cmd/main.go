17abbe0614baafe8079ad701c9dd49b0a170d805
ekko <1146563605@qq.com>
2025-04-27 14:20:09 +0800

    feature: replace ip.

--- cmd/main.go
+++ cmd/main.go
@@ -40,7 +40,7 @@ func HttpServerLifetimeHook(lc fx.Lifecycle, srv *http.Server) {
 // @title						Lumino
 // @version					1.0
 // @description				lumino 记账本服务
-// @host						182.92.152.108:8080
+// @host						101.201.34.183:8080
 // @BasePath					/api/v1
 // @securityDefinitions.apikey	ApiKeyAuth
 // @in							header
@@ -58,4 +58,4 @@ func main() {
 	app.Run()
 }
 
-// 182.92.152.108
+// http://101.201.34.183/
