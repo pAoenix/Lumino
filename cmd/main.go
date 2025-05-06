50dfdaafd900ee0643415ad9b3e975511504c480
ekko <1146563605@qq.com>
2025-05-06 18:07:17 +0800

    feature: chart api

--- cmd/main.go
+++ cmd/main.go
@@ -40,7 +40,7 @@ func HttpServerLifetimeHook(lc fx.Lifecycle, srv *http.Server) {
 // @title						Lumino
 // @version					1.0
 // @description				lumino 记账本服务
-// @host						101.201.34.183:8080
+// @host						https://happyall.xyz
 // @BasePath					/api/v1
 // @securityDefinitions.apikey	ApiKeyAuth
 // @in							header
